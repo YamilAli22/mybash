04066e771f777009a70824d5428dbed3dd0ac958
Yamil <jyamilali22@gmail.com>
2026-05-21 19:38:31 -0300

    execute.c working for simple commands and (at least) one pipe, more testing is needed. Zombie process managment is implemented

--- execute.c
+++ execute.c
@@ -8,6 +8,26 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <assert.h>
+#include <signal.h>
+#include <errno.h>
+
+/*
+Las siguientes dos funciones son para manejar los procesos zombies. Los procesos zombies ocurren cuando un proceso termino pero nunca se recolectaron sus exit code, 
+por lo que el proceso sigue en la process table a pesar de haber terminado. Al llamar a wait() en los procesos que corren en foreground,
+el padre espera a que el hijo termine y su exit code es recolectado, limpiando al proceso hijo de la process table, esto no ocurre cuando uno (o más) procesos se están
+ejecutando en background, por eso registramos un manejador de señal SIGCHLD que se ejecuta automáticamente cuando un hijo termina. 
+Este manejador llama a waitpid con WNOHANG en un loop para recolectar todos los hijos que hayan terminado sin bloquear al padre, limpiándolos de la process table.
+*/ 
+
+static void sigchld_handler(int sig) {
+    int saved_errno = errno;
+    while (waitpid(-1, NULL, WNOHANG) > 0);
+    errno = saved_errno;
+}
+
+void init_signal_handlers(void) {
+    signal(SIGCHLD, sigchld_handler);
+}
 
 void execute_command(scommand s_cmd) {
     assert(s_cmd!=NULL);
@@ -17,15 +37,16 @@ void execute_command(scommand s_cmd) {
         builtin_run(s_cmd);
         return;
     }
-    char **argv = malloc((scommand_length(s_cmd) + 1) * sizeof(char *));
+    unsigned int cmd_length = scommand_length(s_cmd);
+    char **argv = malloc((cmd_length + 1) * sizeof(char *));
     char *filename_in = scommand_get_redir_in(s_cmd);
     char *filename_out = scommand_get_redir_out(s_cmd);
 
-    for (int i = 0; i < scommand_length(s_cmd); ++i) {
+    for (unsigned int i = 0; i < cmd_length; ++i) {
         argv[i] = scommand_front(s_cmd);
         scommand_pop_front(s_cmd);
     }
-    argv[scommand_length(s_cmd)] = NULL;
+    argv[cmd_length] = NULL;
 
     // Todas estas syscalls deberian ser chequeadas en su valor de retorno
     if (filename_in != NULL) {
@@ -42,7 +63,7 @@ void execute_command(scommand s_cmd) {
         free(argv);
         perror("execvp");
         exit(EXIT_FAILURE);
-    }    
+    }
 }
 
 // caso de 2 o mas comandos en el pipeline, acá tenemos que ver si son internos o no
@@ -57,9 +78,10 @@ void execute_pipeline(pipeline apipe) {
     int fd[2];
     int read_fd, write_fd = -1;
 
-    for (int i=0; i<num_cmds; ++i) {
+    for (unsigned int i=0; i<num_cmds; ++i) {
         if (i!=0) {
-            read_fd = fd[0];
+            // para todos los comandos excepto el primero, seteo read_fd y write_fd para que apunten al pipe creado en el paso anterior (antes de crear un nuevo pipe para el paso actual)
+            read_fd = fd[0]; 
             write_fd = fd[1];
         }
 
@@ -72,20 +94,23 @@ void execute_pipeline(pipeline apipe) {
             perror("fork\n");
             return;
 
-        } else if (rc==0) {    
-        if (i!=0) {
-            dup2(read_fd, STDIN_FILENO);
-            close(read_fd);
-            close(write_fd);
+        } else if (rc==0) { // creo que todo esto se podría simplificar usando popen y pclose, pero para entender el funcionamiento es mejor hacerlo así 
+            if (i != num_cmds - 1) {
+                close(fd[0]);
+                dup2(fd[1], STDOUT_FILENO);
+                close(fd[1]);
             }
-        if (i != num_cmds - 1) {
-            close(fd[0]);
-            dup2(fd[1], STDOUT_FILENO);
-            close(fd[1]);
+            if (i!=0) {
+                close(write_fd);
+                dup2(read_fd, STDIN_FILENO);
+                close(read_fd);
             }
-        execute_command(pipeline_front(apipe));
+            execute_command(pipeline_front(apipe));
         }
         else { // padre
+            if (i!=num_cmds-1) {
+                close(fd[1]);
+            }
             if (i!=0) {
                 close(read_fd);
                 close(write_fd);
@@ -94,11 +119,14 @@ void execute_pipeline(pipeline apipe) {
         }
     }
     if (wt) {
-        wait(NULL);
+        for (unsigned int i=0; i<num_cmds; ++i) {
+            wait(NULL);
+        }
     }
-}  
+}
 
-/* Pasos básicos para implementar el piping que encontré en stack overflow:
+/*
+Pasos básicos para implementar el piping que encontré en stack overflow:
 1. Create a pipe.
 2. Execute fork().
 3. In the child: overwrite standard input with the read end of the previous pipe, and standard output with the write end of the current pipe.
@@ -109,5 +137,4 @@ Básicamente la idea es crear un pipe para cada comando, excepto el último, e i
 y mantener la referencia de las puntas del pipe del comando en que estoy actualmente, para que el comando siguiente 
 pueda leer desde la punta del pipe anterior. Osea, pipe del comando i (i>0) lee desde el read end del pipe del comando i-1, escribe lo que tenga que 
 escribir, y "guarda" su read en para que el comando i+1 haga lo mismo que él. Cerras las puntas que ya no se usan.
-
 */
